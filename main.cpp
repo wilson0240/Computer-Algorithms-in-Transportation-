dd0c76eb5694caa982ff1106dfbbdbd1c114c62a
Liu wei-shen <wilson02458@gmail.com>
2020-03-26 21:14:18 +0800

    2020 hw-endend

--- main.cpp
+++ main.cpp
@@ -20,69 +20,88 @@ int main()
     cout<<"賭客"<<endl;
     game(); //呼叫函數
     gambler = sum; //將sum存到gambler
-    if(host == gambler){
+    if(host == gambler){ //判斷誰的點數大
     cout<<"平手，重骰一次"<<endl ;
      goto again;} //goto 指定回到哪一行
-    if(host>gambler){
-    cout<<"莊家點數較大，莊家獲勝"<<endl ;
-    }
+    else if(host>gambler){
+            cout<<"莊家點數較大，莊家獲勝"<<endl ;
+        }
     else{
-    cout<<"賭客點數較大，賭客獲勝"<<endl ;
-    }
-    return 0; //???
+        cout<<"賭客點數較大，賭客獲勝"<<endl ;}
+    return 0;
 }
-void game(void){ //void?
+void game(void){
     do{
     int dice1 = rand( ) %6+ 1; //隨機數除6的餘數+1
     int dice2 = rand( ) %6+ 1;
     int dice3 = rand( ) %6+ 1;
     int dice4 = rand( ) %6+ 1;
     cout<<"骰子點數:"<<dice1<<dice2<<dice3<<dice4<<endl;
-    //判斷骰子得分
-    if(dice1 != dice2 != dice3 != dice4){
-    sum = 0;}//四個都不一樣
-    if(dice1 == dice2){
-        sum = dice1+dice2;} //骰子1.2一樣(共6個)
-    if(dice1 == dice3){
-        sum = dice1+dice3;}
-    if(dice1 == dice4){
-        sum = dice1+dice4;}
-    if(dice2 == dice3){
-        sum = dice2+dice3;}
-    if(dice2 == dice4){
-        sum = dice2+dice4;}
-    if(dice3 == dice4){
-        sum = dice3+dice4;}
-    if((dice1 == dice2) && (dice3 == dice4)){
+//判斷骰子得分
+    if(dice1==dice4 && dice2==dice4 && dice3==dice4){
+    sum = 10*dice1+10*dice2+10*dice3+10*dice4;
+    cout<<"Bow-Gi"<<endl;}  //四個一樣分數相加
+    else if(dice1 == dice2 && dice1 == dice3){
+    sum = 0;
+    cout<<"重骰"<<endl;} //3個一樣時(4種)
+    else if(dice1 == dice2 && dice1 == dice4){
+    sum = 0;
+    cout<<"重骰"<<endl;}
+    else if(dice1 == dice3 && dice1 == dice4){
+    sum = 0;
+    cout<<"重骰"<<endl;}
+    else if(dice2 == dice3 && dice2== dice4){
+    sum = 0;
+    cout<<"重骰"<<endl;}
+    else if((dice1 == dice2) && (dice3 == dice4)){
         if(dice1 > dice3){
             sum = dice1 + dice2; }//兩對時比大小(三種)
         else{
             sum = dice3 + dice4; }
     }
-    if((dice1 == dice3) && (dice2 == dice4)){
+    else if((dice1 == dice3) && (dice2 == dice4)){
         if(dice1 > dice2){
             sum = dice1 + dice3;}
         else{
             sum = dice2 + dice4;}
     }
-    if((dice1 == dice4) && (dice2 == dice3)){
+    else if((dice1 == dice4) && (dice2 == dice3)){
         if(dice1 > dice2){
             sum = dice1 + dice4;}
         else{
             sum = dice2 + dice3;}
     }
-    if(dice1 == dice2 && dice1 == dice3){
-    sum = 0; } //3個一樣時(4種)
-    if(dice1 == dice2 && dice1 == dice4){
-    sum = 0; }
-    if(dice1 == dice3 && dice1 == dice4){
-    sum = 0; }
-    if(dice2 == dice3 && dice2== dice4){
-    sum = 0; }
-    if(dice1==dice4 && dice2==dice4 && dice3==dice4){
-    sum = dice1+dice2+dice3+dice4;}  //四個一樣分數相加
-    cout<<"    得分:";
-    cout<<sum<<endl<<endl; //輸出得分
+    else if(dice1 == dice2){
+        sum = dice3+dice4;} //骰子兩個數值一樣(共6種)
+    else if(dice1 == dice3){
+        sum = dice2+dice4;}
+    else if(dice1 == dice4){
+        sum = dice2+dice3;}
+    else if(dice2 == dice3){
+        sum = dice1+dice4;}
+    else if(dice2 == dice4){
+        sum = dice1+dice3;}
+    else if(dice3 == dice4){
+        sum = dice1+dice2;}
+    else if(dice1 != dice2) //判斷四個都不一樣
+    {
+     if(dice1 != dice3)
+        {
+         if(dice1 != dice4)
+            {
+             if(dice2 != dice3)
+                {
+                 if(dice2 != dice4)
+                    {
+                    if(dice3 != dice4)
+                        {sum = 0;
+                        cout<<"重骰"<<endl;}
+                    }
+                }
+            }
+         }
+    }
+    cout<<"    得分:"<<sum<<endl<<endl; //輸出得分
     }while (sum ==0); //do while 如果sum等於0 重新執行迴圈
     return ;
 }
